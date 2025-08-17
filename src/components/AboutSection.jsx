00db1308cdb2035c141ec475ccccb39d42e6fba1
Basit46I <basitprogrammer46@gmail.com>
2025-08-17 20:01:29 +0500

    sixth commit

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -29,7 +29,7 @@ export const AboutSection = () => {
             </p>
 
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
-              <a href="#contact" className="cosmic-button">
+              <a href="/cv/abdul-basit.pdf" target="_blank" className="cosmic-button">
                 {" "}
                 Download CV
               </a>
